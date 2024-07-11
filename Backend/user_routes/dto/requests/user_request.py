b287c626d355fbd1d5c36de071f8d507c0b86580
Thomas170491 <thomaspapas470@gmail.com>
2024-07-11 20:06:31 +0200

    Added user routes

---
+++ Backend/user_routes/dto/requests/user_request.py
@@ -0,0 +1,24 @@
+from marshmallow import Schema, fields, validate
+
+class LoginRequestDTO(Schema):
+    username = fields.String(required=True, validate=validate.Length(min=1))
+    password = fields.String(required=True, validate=validate.Length(min=1))
+    remember_me = fields.Bool(missing=False)
+
+class OrderRideRequestDTO(Schema):
+    name = fields.String(required=True, validate=validate.Length(min=1))
+    departure = fields.String(required=True, validate=validate.Length(min=1))
+    destination = fields.String(required=True, validate=validate.Length(min=1))
+    time = fields.DateTime(required=True)
+
+class CalculatePriceRequestDTO(Schema):
+    departure = fields.String(required=True, validate=validate.Length(min=1))
+    destination = fields.String(required=True, validate=validate.Length(min=1))
+
+class CreatePaymentRequestDTO(Schema):
+    departure = fields.String(required=True, validate=validate.Length(min=1))
+    destination = fields.String(required=True, validate=validate.Length(min=1))
+
+class ExecutePaymentRequestDTO(Schema):
+    paymentId = fields.String(required=True)
+    PayerID = fields.String(required=True)
