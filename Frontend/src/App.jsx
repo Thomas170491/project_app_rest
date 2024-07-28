f76770a38a79d348453e1abc14b0dd0d31308df3
Thomas170491 <thomaspapas470@gmail.com>
2024-07-28 08:58:52 +0200

    Added header token requirement to DTO

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,7 @@ import DriverDashboard from './components/DriverDashboard';
 import Dashboard from './components/UserDashboard';
 import UserLogin from './components/UserLogin';
 import OrderRide from './components/OrderRide';
+import OrderStatus from './components/OrderStatus'
 import RideAccepted from './components/DriverAcceptRide';
 import DeclineRide from './components/DriverDeclineRide';
 import { UserProvider } from './components/UserContext';
@@ -22,13 +23,17 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/admin/dashboard" element={<ProtectedRoute component={AdminDashboard} roles={['admin']} />} />  */}
-          <Route path="/drivers/dashboard" element={<ProtectedRoute component={DriverDashboard} roles={['driver']} />} />
-          <Route path="/users/dashboard" element={<ProtectedRoute component={Dashboard} roles={['user']} />} />
           <Route path="/users/login" element={<UserLogin />} />
-          <Route path="/drivers/accept_ride/:rideId" element={< RideAccepted />} />
-          <Route path="/drivers/decline_ride/:rideId" element={<DeclineRide />} />
-          <Route path="/users/order-ride" element={<ProtectedRoute component={OrderRide} roles={['user']} />} />
+          {/* <Route path="/admin/dashboard" element={<ProtectedRoute component={AdminDashboard} roles={['admin']} />} />  */}
+          <Route path="/users/dashboard" element={<ProtectedRoute component={Dashboard} roles={['user']} />} />
+          <Route path="/users/order_ride" element={<ProtectedRoute component={OrderRide} roles={['user']} />} />
+          <Route path = '/users/order_status/:rideId' element={<ProtectedRoute component={OrderStatus} roles={['user']}/>} />
+          <Route path="/drivers/accept_ride/:rideId" element={<ProtectedRoute component={RideAccepted} roles= {['driver']} />} />
+          <Route path="/drivers/dashboard" element={<ProtectedRoute component={DriverDashboard} roles={['driver']} />} />
+          <Route path="/drivers/decline_ride/:rideId" element={<ProtectedRoute component={DeclineRide} roles={['driver']} />} />
+          <Route path='/unauthorized' element ={<Unauthorized />} />
+          
+
         </Routes>
       </UserProvider>
     </Router>
