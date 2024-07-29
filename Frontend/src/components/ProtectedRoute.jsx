80a7b64fcf1dd4f0b2df0b68a1933e2f904826ae
Thomas <thomaspapas470@gmail.com>
2024-07-29 23:23:32 +0200

    Last version

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children, role }) => {
+export const ProtectedRoute = ({ children, role }) => {
   const token = localStorage.getItem("access_token");
-  const user = localStorage.getItem("user");
+  const user = JSON.parse(localStorage.getItem("user"));
 
   if (!user || !token) {
     return <Navigate to="/users/login" />;
   }
 
-  // if (role && user.role !== role) {
-  //   return <Navigate to="/unauthorized" />;
-  // }
+  if (role && user.role !== role) {
+    return <Navigate to="/unauthorized" />;
+  }
 
   return children;
 };
 
-export default ProtectedRoute;
+export const PublicRoute = ({ children }) => {
+  const token = localStorage.getItem("access_token");
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  if (token) {
+    // Redirect based on user role or a default page
+    if (user && user.role === "admin") {
+      return <Navigate to="/admin/dashboard" />;
+    }
+    if (user && user.role === "driver") {
+      return <Navigate to="/drivers/dashboard" />;
+    }
+    return <Navigate to="/users/dashboard" />;
+  }
+
+  return children;
+};
